933cf977c176418e0ff0c38be346d9759f937599
haidy <ren.hong@northeastern.edu>
2025-06-16 11:17:13 -0700

    a6

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ const CONNECTION_STRING =
   process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kambaz";
 mongoose.connect(CONNECTION_STRING);
 const app = express() 
+app.set("trust proxy", 1);
 app.use(cors(
     {credentials: true, 
     origin: [
