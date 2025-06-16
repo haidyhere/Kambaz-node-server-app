4c6bd26641997fa64e18957415f9130ce871bf0f
haidy <ren.hong@northeastern.edu>
2025-06-16 03:26:24 -0700

    a6

--- index.js
+++ index.js
@@ -60,4 +60,6 @@ ModuleRoutes(app);
 AssignmentRoutes(app);
 EnrollmentRoutes(app);
 
-app.listen(process.env.PORT || 4000)
+app.listen(process.env.PORT || 4000, ()=>{
+    console.log(`Server running on port ${process.env.PORT || 4000}`);
+})
